d69c28a068444dfd2373271332155c38c71cd76d
lr580 <2277379149@qq.com>
2023-07-03 17:33:19 +0800

    实现了Tuple类，完成Exercise1

--- src/java/simpledb/storage/Tuple.java
+++ src/java/simpledb/storage/Tuple.java
@@ -1,8 +1,11 @@
 package simpledb.storage;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.ArrayList;
+//import java.util.Arrays;
 import java.util.Iterator;
+
+import simpledb.common.Type;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -10,30 +13,42 @@ import java.util.Iterator;
  * with the data for each field.
  */
 public class Tuple implements Serializable {
+    private TupleDesc tupleDesc;
+    private ArrayList<Field> fields;
 
     private static final long serialVersionUID = 1L;
 
     /**
      * Create a new tuple with the specified schema (type).
      *
-     * @param td the schema of this tuple. It must be a valid TupleDesc
-     *           instance with at least one field.
+     * @param td the schema of this tuple. It must be a valid TupleDesc instance
+     *           with at least one field.
      */
     public Tuple(TupleDesc td) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        tupleDesc = td;
+        fields = new ArrayList<>(td.numFields());
+        for (int i = 0, n = td.numFields(); i < n; ++i) {
+            // 可以改更优雅，但暂时没必要
+            if (td.getFieldType(i) == Type.INT_TYPE) {
+                fields.add(new IntField(0));
+            } else {
+                fields.add(new StringField("", Type.STRING_LEN));
+            }
+        }
     }
 
     /**
      * @return The TupleDesc representing the schema of this tuple.
      */
     public TupleDesc getTupleDesc() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return tupleDesc;
     }
 
     /**
-     * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
+     * @return The RecordId representing the location of this tuple on disk. May be
+     *         null.
      */
     public RecordId getRecordId() {
         // TODO: some code goes here
@@ -56,7 +71,8 @@ public class Tuple implements Serializable {
      * @param f new value for the field.
      */
     public void setField(int i, Field f) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        fields.set(i, f);
     }
 
     /**
@@ -64,35 +80,47 @@ public class Tuple implements Serializable {
      * @return the value of the ith field, or null if it has not been set.
      */
     public Field getField(int i) {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return fields.get(i);
     }
 
     /**
-     * Returns the contents of this Tuple as a string. Note that to pass the
-     * system tests, the format needs to be as follows:
+     * Returns the contents of this Tuple as a string. Note that to pass the system
+     * tests, the format needs to be as follows:
      * <p>
      * column1\tcolumn2\tcolumn3\t...\tcolumnN
      * <p>
      * where \t is any whitespace (except a newline)
      */
     public String toString() {
-        // TODO: some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        // DONE: some code goes here
+//        throw new UnsupportedOperationException("Implement this");
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (Field f : fields) {
+            if (!first) {
+                sb.append('\t');
+            } else {
+                first = false;
+            }
+            sb.append(f.toString());
+        }
+        return sb.toString();
     }
 
     /**
      * @return An iterator which iterates over all the fields of this tuple
      */
     public Iterator<Field> fields() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return fields.iterator();
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
      */
     public void resetTupleDesc(TupleDesc td) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        tupleDesc = td;// 都说only affecting the TupleDesc了就不动fields了
     }
 }
