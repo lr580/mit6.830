e53b6ff68277fa51284491c28f58ec7746f4c933
lr580 <2277379149@qq.com>
2023-07-06 18:40:36 +0800

    完成 Predicate 和 Filter 类编写并通过对应单元测试

--- src/java/simpledb/execution/Filter.java
+++ src/java/simpledb/execution/Filter.java
@@ -14,64 +14,82 @@ public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final Predicate p;
+    private OpIterator child;
+
     /**
-     * Constructor accepts a predicate to apply and a child operator to read
-     * tuples to filter from.
+     * Constructor accepts a predicate to apply and a child operator to read tuples
+     * to filter from.
      *
      * @param p     The predicate to filter tuples with
      * @param child The child operator
      */
     public Filter(Predicate p, OpIterator child) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        this.p = p;
+        this.child = child;
     }
 
     public Predicate getPredicate() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return p;
     }
 
     public TupleDesc getTupleDesc() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return child.getTupleDesc();
     }
 
-    public void open() throws DbException, NoSuchElementException,
-            TransactionAbortedException {
-        // TODO: some code goes here
+    public void open() throws DbException, NoSuchElementException, TransactionAbortedException {
+        // DONE: some code goes here
+        child.open();
+        super.open();
     }
 
     public void close() {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        child.rewind();
     }
 
     /**
      * AbstractDbIterator.readNext implementation. Iterates over tuples from the
-     * child operator, applying the predicate to them and returning those that
-     * pass the predicate (i.e. for which the Predicate.filter() returns true.)
+     * child operator, applying the predicate to them and returning those that pass
+     * the predicate (i.e. for which the Predicate.filter() returns true.)
      *
-     * @return The next tuple that passes the filter, or null if there are no
-     *         more tuples
+     * @return The next tuple that passes the filter, or null if there are no more
+     *         tuples
      * @see Predicate#filter
      */
-    protected Tuple fetchNext() throws NoSuchElementException,
-            TransactionAbortedException, DbException {
-        // TODO: some code goes here
-        return null;
+    protected Tuple fetchNext()
+            throws NoSuchElementException, TransactionAbortedException, DbException {
+        // DONE: some code goes here
+        for (;;) {
+            if (!child.hasNext()) {
+                return null;
+            }
+            Tuple t = child.next();
+            if (p.filter(t)) {
+                return t;
+            }
+        }
     }
 
     @Override
     public OpIterator[] getChildren() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return new OpIterator[] { child };
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        child = children[0];
     }
 
 }
--- src/java/simpledb/execution/Predicate.java
+++ src/java/simpledb/execution/Predicate.java
@@ -47,6 +47,10 @@ public class Predicate implements Serializable {
         }
 
     }
+    
+    private final int field;
+    private final Op op;
+    private final Field operand;
 
     /**
      * Constructor.
@@ -56,31 +60,34 @@ public class Predicate implements Serializable {
      * @param operand field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        this.field = field;
+        this.op = op;
+        this.operand = operand;
     }
 
     /**
      * @return the field number
      */
     public int getField() {
-        // TODO: some code goes here
-        return -1;
+        // DONE: some code goes here
+        return field;
     }
 
     /**
      * @return the operator
      */
     public Op getOp() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return op;
     }
 
     /**
      * @return the operand
      */
     public Field getOperand() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return operand;
     }
 
     /**
@@ -93,8 +100,8 @@ public class Predicate implements Serializable {
      * @return true if the comparison is true, false otherwise.
      */
     public boolean filter(Tuple t) {
-        // TODO: some code goes here
-        return false;
+        // DONE: some code goes here
+        return t.getField(field).compare(op, operand);
     }
 
     /**
@@ -102,7 +109,7 @@ public class Predicate implements Serializable {
      * operand_string"
      */
     public String toString() {
-        // TODO: some code goes here
-        return "";
+        // DONE: some code goes here
+        return "f = " + field + " op = " + op + " operand = " + operand;
     }
 }
