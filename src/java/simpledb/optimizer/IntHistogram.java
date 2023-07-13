10b2966f47e075e84dcdf277c909e6845938840a
lr580 <2277379149@qq.com>
2023-07-13 16:18:02 +0800

    完成了 Lab3.Exercise1

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -1,30 +1,50 @@
 package simpledb.optimizer;
 
+import java.util.Arrays;
+
 import simpledb.execution.Predicate;
 
 /**
- * A class to represent a fixed-width histogram over a single integer-based field.
+ * A class to represent a fixed-width histogram over a single integer-based
+ * field.
  */
 public class IntHistogram {
+    private final int cnt[], numb, min, max, w;
+    private int ntups;
 
     /**
      * Create a new IntHistogram.
      * <p>
-     * This IntHistogram should maintain a histogram of integer values that it receives.
-     * It should split the histogram into "buckets" buckets.
+     * This IntHistogram should maintain a histogram of integer values that it
+     * receives. It should split the histogram into "buckets" buckets.
      * <p>
-     * The values that are being histogrammed will be provided one-at-a-time through the "addValue()" function.
+     * The values that are being histogrammed will be provided one-at-a-time through
+     * the "addValue()" function.
      * <p>
      * Your implementation should use space and have execution time that are both
-     * constant with respect to the number of values being histogrammed.  For example, you shouldn't
-     * simply store every value that you see in a sorted list.
+     * constant with respect to the number of values being histogrammed. For
+     * example, you shouldn't simply store every value that you see in a sorted
+     * list.
      *
      * @param buckets The number of buckets to split the input value into.
-     * @param min     The minimum integer value that will ever be passed to this class for histogramming
-     * @param max     The maximum integer value that will ever be passed to this class for histogramming
+     * @param min     The minimum integer value that will ever be passed to this
+     *                class for histogramming
+     * @param max     The maximum integer value that will ever be passed to this
+     *                class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        numb = Math.min(buckets, max - min + 1);
+        cnt = new int[numb];
+        this.min = min;
+        this.max = max;
+        ntups = 0;
+        // ceil((max-min+1)/numb)
+        w = (max - min) / numb + 1;
+    }
+
+    private int loc(int v) {
+        return (v - min) / w;
     }
 
     /**
@@ -33,14 +53,16 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        ++cnt[loc(v)];
+        ++ntups;
     }
 
     /**
      * Estimate the selectivity of a particular predicate and operand on this table.
      * <p>
-     * For example, if "op" is "GREATER_THAN" and "v" is 5,
-     * return your estimate of the fraction of elements that are greater than 5.
+     * For example, if "op" is "GREATER_THAN" and "v" is 5, return your estimate of
+     * the fraction of elements that are greater than 5.
      *
      * @param op Operator
      * @param v  Value
@@ -48,19 +70,56 @@ public class IntHistogram {
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
 
-        // TODO: some code goes here
+        // DONE: some code goes here
+        if (op == Predicate.Op.EQUALS) {
+            if (v < min || v > max) {
+                return 0.0;
+            }
+            int i = loc(v);
+            return 1.0 * cnt[i] / w / ntups;
+        }
+        if (op == Predicate.Op.NOT_EQUALS) {
+            return 1.0 - estimateSelectivity(Predicate.Op.EQUALS, v);
+        }
+        if (op == Predicate.Op.GREATER_THAN) {
+            if (v > max) {
+                return 0.0;
+            }
+            if (v < min) {
+                return 1.0;
+            }
+            int i = loc(v);
+            int br = (i + 1) * w - 1 + min;
+            double res = 1.0 * (br - v) / w;
+            res *= 1.0 * cnt[i] / ntups;
+            for (int j = i + 1; j < numb; ++j) {
+                res += 1.0 * cnt[j] / w / ntups;
+            }
+            return res;
+        }
+        if (op == Predicate.Op.GREATER_THAN_OR_EQ) {
+            return estimateSelectivity(Predicate.Op.EQUALS, v)
+                    + estimateSelectivity(Predicate.Op.GREATER_THAN, v);
+        }
+        if (op == Predicate.Op.LESS_THAN) {
+            return 1.0 - estimateSelectivity(Predicate.Op.GREATER_THAN_OR_EQ, v);
+        }
+        if (op == Predicate.Op.LESS_THAN_OR_EQ) {
+            return 1.0 - estimateSelectivity(Predicate.Op.GREATER_THAN, v);
+        }
         return -1.0;
     }
 
     /**
      * @return the average selectivity of this histogram.
      *         <p>
-     *         This is not an indispensable method to implement the basic
-     *         join optimization. It may be needed if you want to
-     *         implement a more efficient optimization
+     *         This is not an indispensable method to implement the basic join
+     *         optimization. It may be needed if you want to implement a more
+     *         efficient optimization
      */
     public double avgSelectivity() {
-        // TODO: some code goes here
+        // DONE: some code goes here
+        // skip it
         return 1.0;
     }
 
@@ -68,7 +127,18 @@ public class IntHistogram {
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // TODO: some code goes here
-        return null;
+        // DONE: some code goes here
+        return "IntHistogram NumB=" + numb + ",min=" + min + ",max=" + max + ",cnt="
+                + Arrays.toString(cnt);
     }
+
+//    public static void main(String[] args) {
+//        IntHistogram h = new IntHistogram(10, 1, 10);
+//        h.addValue(3);
+//        h.addValue(3);
+//        h.addValue(3);
+//        h.addValue(1);
+//        h.addValue(10);
+//        System.out.println(h.estimateSelectivity(Predicate.Op.GREATER_THAN_OR_EQ, 3));
+//    }
 }
